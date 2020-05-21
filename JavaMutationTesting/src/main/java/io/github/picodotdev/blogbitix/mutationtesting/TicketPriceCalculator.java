ef0de7e4d80fb70c1fc235a1e8912bd70dfb6c2a
Tona Romera <tonaromera.ext@goldcar.com>
2020-05-21 19:09:01 +0200

    prueba develop

--- JavaMutationTesting/src/main/java/io/github/picodotdev/blogbitix/mutationtesting/TicketPriceCalculator.java
+++ JavaMutationTesting/src/main/java/io/github/picodotdev/blogbitix/mutationtesting/TicketPriceCalculator.java
@@ -13,6 +13,7 @@ public class TicketPriceCalculator {
         double price = countAdults(passengers) * adultTicketPrice + countChildrens(passengers) * childTicketPrice;
         double discount = (isFamily(passengers)) ? FAMILY_DISCOUNT : 0d;
         System.out.println("CalculatePrice:"+price);
+        System.out.println("DEVELOP");
         return price * (1 - discount);
     }
 
